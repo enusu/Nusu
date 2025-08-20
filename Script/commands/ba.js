c35d9fba104ec47a2d591edd1b369ab46058bf67
enusu <shagorh714@gmail.com>
2025-08-20 11:21:26 +0600

    Update ba.js

--- Script/commands/ba.js
+++ Script/commands/ba.js
@@ -45,7 +45,7 @@ const emojiAudioMap = {
  "😩": "https://files.catbox.moe/b4m5aj.mp3",
  "🫣": "https://files.catbox.moe/ttb6hi.mp3",
  "🐸": "https://files.catbox.moe/utl83s.mp3"
- "Ebrahim": https://drive.google.com/uc?export=download&id=1WuC0YUrWlE9XiVDSMZyt1ql5U20IcBoE"
+ "Ebrahim": "https://drive.google.com/uc?export=download&id=1WuC0YUrWlE9XiVDSMZyt1ql5U20IcBoE"
 };
 
 module.exports.handleEvent = async ({ api, event }) => {
